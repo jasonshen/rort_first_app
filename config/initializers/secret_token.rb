33472c2b9f95bd7af9a48ff70b7086a98c1ae8c1
Jason Shen <jasonyshen@gmail.com>
2013-02-07 11:28:20 -0800

    Initial commit with the Ruby on Rails Tutorial book by Michael Hartl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RortFirstApp::Application.config.secret_token = '8f35de9a6b62ff6954e2168b2d53aa9dbaa46bc6fd8a79163e931af6b8f72b90f4d88e170d5212c47a2f24d4c3e33a27e8cdcab308c753e1c3d8ee748648c8dc'
